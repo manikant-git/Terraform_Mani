798ad71d310803d318f54c526aefc4bc86051349
manikant-git <--manikanta14568@gmail.com>
2025-07-28 12:31:31 +0530

    my

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -48,5 +48,7 @@ resource "aws_route" "igw" {
 
 resource "aws_route_table_association" "public" {
   count          = length(var.public_subnets)
-  subnet_id
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
 
